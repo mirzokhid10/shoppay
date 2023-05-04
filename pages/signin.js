c40d322bbcd7c56ebf879239847199cbbc905548
mirzokhid10 <mxudoyberdiyev21@gmail.com>
2023-05-04 13:08:54 +0300

    added: signpage

--- pages/signin.js
+++ pages/signin.js
@@ -1,3 +1,75 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import styles from "../styles/signin.module.scss";
+import { BiLeftArrowAlt } from "react-icons/bi";
+import Link from "next/link";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+import LoginInput from "@/components/inputs/loginInput";
+import { useState } from "react";
+const initialvalues = {
+  login_email: "",
+  login_password: "",
+};
+
 export default function signin() {
-  return <div>signin</div>;
+  const [user, setUser] = useState(initialvalues);
+  const { login_email, login_password } = user;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  const loginValidation = Yup.object({
+    login_email: Yup.string()
+      .required("Email address is required")
+      .email("Please enter a valid email address"),
+
+    login_password: Yup.string().required("Please enter a password"),
+  });
+  return (
+    <>
+      <Header country="Uzbekistan" />
+      <div className={styles.login}>
+        <div className={styles.login__container}>
+          <div className={styles.login__header}>
+            <div className={styles.back__svg}>
+              <BiLeftArrowAlt />
+            </div>
+            <span>
+              We'd be happy to join us! <Link href="/">Go store</Link>
+            </span>
+          </div>
+          <div className={styles.login__form}>
+            <h1>Sign in</h1>
+            <p>Get access to one of the best eshopping services in the world</p>
+            <Formik
+              enableReinitialize
+              initialvalues={{ login_email, login_password }}
+              validationSchema={loginValidation}
+            >
+              {(form) => (
+                <Form>
+                  <LoginInput
+                    type="text"
+                    name="login_email"
+                    icon="email"
+                    placeholder="Email address"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="login_password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </div>
+      </div>
+      <Footer country="Uzbekistan" />
+    </>
+  );
 }
